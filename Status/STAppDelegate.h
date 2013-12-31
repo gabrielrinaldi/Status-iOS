0bdea2706054bc523cee9682d5144e905b8b18ae
Gabriel Rinaldi <gabriel_rinaldi@mac.com>
2013-12-30 16:37:37 -0800

    Initial import

---
+++ Status/STAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  STAppDelegate.h
+//  Status
+//
+//  Created by Gabriel Rinaldi on 12/30/13.
+//  Copyright (c) 2013 Gabriel Rinaldi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
